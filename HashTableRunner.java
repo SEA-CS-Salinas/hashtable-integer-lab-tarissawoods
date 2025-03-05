ce09198dcda8c9588c05fbae467152a280fd6bab
tarissawoods <132471195+tarissawoods@users.noreply.github.com>
2025-03-05 05:43:43 -0600

    Update HashTableRunner.java

--- HashTableRunner.java
+++ HashTableRunner.java
@@ -1,29 +1,48 @@
-//(c) A+ Computer Science
-//www.apluscompsci.com
-
-//Name -
-
 import java.util.LinkedList;
 import java.util.Scanner;
 import java.io.File;
 import static java.lang.System.*;
+import java.util.*;
 
 public class HashTableRunner
 {
   public static void main ( String[] args )
   {
-		try{
-			//make a new table			
+/**
+Createing scanner to read the numbers.dat file
+*/
+      String filePath = "numbers.dat";
+      File file = new File(filePath);
+      int bucketNum  = 10;
+		//Scanner bum = new Scanner(file);
+		try(Scanner bum = new Scanner(file)){
+		    System.out.println("LOOKING FOR FILE...");
+			//make a new table		
+			Hashtable<Integer, LinkedList<Integer>> hashTab = new Hashtable<>();
+			ArrayList<Integer> numbers = new ArrayList<Integer>();
+			for (int i = 0; i < bucketNum; i++){
+			    hashTab.put(i, new LinkedList<>());
+			}
+			//read from the file
+			while (bum.hasNextLine()){
+			    String line = bum.nextLine().trim();
+			    //ignoring empty lines
+			    if (!line.isEmpty()){
+			        int lum = Integer.parseInt(line);
+                    int bucketInd = lum % bucketNum; // Determine bucket
+                    hashTab.get(bucketInd).add(lum);
+			    }
+			}
 			
-			//read from the file			
-			
-			//load stuff into the table			
-			
-			//print out the table
+			System.out.println("HashTable Buckets:");
+            for (int j = 0; j < bucketNum; j++) {
+                System.out.println("Bucket " + j + ": " + hashTab.get(j));
+            }
 		}
 		catch(Exception e)
 		{
 			System.out.println("Houston, we have a problem!");
+			System.out.println(e.getMessage());
 		}
   }
 }
